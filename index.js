44c67bc62b96776334bb87b85266241f7e951b3c
vagmr <2737165206.qqcom>
2023-10-09 20:35:43 +0800

    test

--- index.js
+++ index.js
@@ -1,47 +1,56 @@
-const EXPRESS=require('express')
-const app=EXPRESS()
+const EXPRESS = require('express')
+const app = EXPRESS()
 
-const listStoryR=require("./router/listStory")
-const listAnnR=require("./router/announcementList")
-const forumList=require("./router/forumList")
-const novelView=require("./router/novelView")
-const novelDetail=require("./router/novelDetail")
-const userInfo=require("./router/userInfo")
-const image=require("./router/image")
-const Login=require("./router/login")
+const listStoryR = require("./router/listStory")
+const listAnnR = require("./router/announcementList")
+const forumList = require("./router/forumList")
+const novelView = require("./router/novelView")
+const novelDetail = require("./router/novelDetail")
+const userInfo = require("./router/userInfo")
+const image = require("./router/image")
+const Login = require("./router/login")
+const blog = require("./router/blog");
 
 //中间件配置
 //适应Post请求
 app.use(EXPRESS.json())
 //适应get请求
-app.use(EXPRESS.urlencoded({extended:false}))
+app.use(EXPRESS.urlencoded({ extended: false }))
 
 //静态文件托管
 // app.use('/static',EXPRESS.static("static"))
 
 //路由中间件
-app.use("/",listStoryR)
-app.use("/",listAnnR)
-app.use("/",forumList)
-app.use("/",novelView)
-app.use("/",userInfo)
-app.use("/",novelDetail)
-app.use("/",image)
-app.use("/",Login)
+app.use("/", listStoryR)
+app.use("/", listAnnR)
+app.use("/", forumList)
+app.use("/", novelView)
+app.use("/", userInfo)
+app.use("/", novelDetail)
+app.use("/", image)
+app.use("/", Login)
+app.use("/", blog);
 
-app.get("/",(req, res)=>{
+app.get("/", (req, res) => {
     res.send(`<div style="position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);">
                     <h1>welcome this project</h1>
                     <div>post-llist</div>
                     <ul style="list-style: none;padding-left: 0;">
-                        <li><a href="https://express-vercel-ruby.vercel.app/forumList">/forumList</a></li>
-                        <li><a href="https://express-vercel-ruby.vercel.app/listStory">/listStory</a></li>
-                        <li><a href="https://express-vercel-ruby.vercel.app//novelView">/novelView</a></li>
+                        <li><a href="https://express-vercel-eight-delta.vercel.app/forumList">/forumList</a></li>
+                        <li><a href="https://express-vercel-eight-delta.vercel.app/listStory">/listStory</a></li>
+                        <li><a href="https://express-vercel-eight-delta.vercel.app/blog">/novelView</a></li>
                     </ul>
                     </div>
     `)
 })
 
-app.listen(3001,()=>{
-    console.log('服务器启动成功')
+app.listen(3001, async () => {
+    console.log('Example app listening on port 3000!');
+    try {
+        await mongo.connect(`mongodb+srv://vagmr:vagmr@atlascluster.hbgdlkf.mongodb.net/`);
+        console.log("连接成功");
+    } catch (error) {
+        console.error("连接失败:", error);
+        // 在连接失败时执行其他操作，例如关闭应用程序或发送通知
+    }
 })
---
+++ router/blog.js
@@ -0,0 +1,91 @@
+var express = require('express');
+const router = express.Router();
+//使用token保护接口
+
+const blogModel = require('../tempelate/models/blogModel.js');
+
+//定义中间件
+
+//查询全部博客
+router.get('/blog', function (req, res, next) {
+    blogModel.find().sort({ id: -1 }).then(list => {
+        console.log(req.info);
+        res.json({
+            code: "0000",
+            msg: "查询成功",
+            data: list
+        })
+    }).catch(err => {
+        res.json({
+            code: '0001',
+            msg: '查询失败',
+            data: err
+        })
+    })
+
+})
+router.get('/blog/:id', function (req, res, next) {
+    qid = req.params.id
+    blogModel.findById(qid).then(list => {
+        res.json({
+            code: "0000",
+            msg: "查询成功",
+            data: list
+        })
+    }).catch(err => {
+        res.json({
+            code: '0001',
+            msg: '查询失败',
+            data: err
+        })
+    })
+})
+router.post('/blog', function (req, res, next) {
+    blogModel.create({ ...req.body }).then((result) => {
+        res.json({
+            code: '0000',
+            msg: '添加成功',
+            data: result
+        })
+    }).catch(err => {
+        res.json({ code: '0001', msg: '添加失败', data: err })
+    })
+
+})
+
+router.delete('/blog/:id', (req, res) => {
+    let qid = req.params.id;
+    blogModel.deleteOne({ _id: qid }).then((result) => {
+        res.json({
+            code: '0001',
+            msg: '删除成功',
+            data: result
+        })
+    }).catch(() => {
+        res.json({
+            code: '0000',
+            msg: '删除失败',
+            data: null
+        })
+    }
+    );
+
+})
+router.patch('/blog/:id', (req, res) => {
+    let qid = req.params.id;
+    blogModel.updateOne({ _id: qid }, req.body).then((result) => {
+        res.json({
+            code: '0001',
+            msg: '修改成功',
+            data: result
+        })
+    }).catch(() => {
+        res.json({
+            code: '0000',
+            msg: '修改失败',
+            data: null
+        })
+    })
+})
+
+module.exports = router;

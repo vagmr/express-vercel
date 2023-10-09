49bfbdb548d1b0e759708d26184b300ecbe21dcf
vagmr <2737165206.qqcom>
2023-10-09 22:01:14 +0800

    test

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const EXPRESS = require('express')
 const app = EXPRESS()
 
 const listStoryR = require("./router/listStory")
-const listAnnR = require("./router/announcementList")
 const forumList = require("./router/forumList")
 const novelView = require("./router/novelView")
 const novelDetail = require("./router/novelDetail")
@@ -23,7 +22,6 @@ app.use(EXPRESS.urlencoded({ extended: false }))
 
 //路由中间件
 app.use("/", listStoryR)
-app.use("/", listAnnR)
 app.use("/", forumList)
 app.use("/", novelView)
 app.use("/", userInfo)
@@ -33,16 +31,8 @@ app.use("/", Login)
 app.use("/", blog);
 
 app.get("/", (req, res) => {
-    res.send(`<div style="position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);">
-                    <h1>welcome this project</h1>
-                    <div>post-llist</div>
-                    <ul style="list-style: none;padding-left: 0;">
-                        <li><a href="https://express-vercel-eight-delta.vercel.app/forumList">/forumList</a></li>
-                        <li><a href="https://express-vercel-eight-delta.vercel.app/listStory">/listStory</a></li>
-                        <li><a href="https://express-vercel-eight-delta.vercel.app/blog">/novelView</a></li>
-                    </ul>
-                    </div>
-    `)
+
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(3001, () => {
